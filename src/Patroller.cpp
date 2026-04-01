e473a25e031cf7963736e5463fc0202ea5d620a1
Andreu Corden Moragrega <andreu.corden@gmail.com>
2026-04-01 18:12:29 +0200

    Level Design

--- src/Patroller.cpp
+++ src/Patroller.cpp
@@ -156,14 +156,11 @@ void Patroller::update(int deltaTime, const std::vector<Weight*> weights)
     float pT = position.y + tileMapDispl.y;
     float pB = pT + spriteHeight + tileMapDispl.y;
 
-    cout << '\n' << pL << ' ' << pR << ' ' << pT << ' ' << pB << "Here" << '\n';
-
     for (Weight* w : weights) {
         float wL = w->getPosition().x;
         float wR = wL + 16;
         float wT = w->getPosition().y;
         float wB = wT + 16;
-        cout << wL << ' ' << wR << ' ' << wT << ' ' << wB << '\n';
 
         if (pB > wT && pT < wB) {
             if (moveRight) {
