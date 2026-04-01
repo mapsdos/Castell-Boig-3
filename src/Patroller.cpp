492753d7ac677785e7c05d9024c04caee9b69237
Andreu Corden Moragrega <andreu.corden@gmail.com>
2026-04-01 15:05:06 +0200

    BombFix

--- src/Patroller.cpp
+++ src/Patroller.cpp
@@ -79,7 +79,6 @@ void Patroller::update(int deltaTime, const std::vector<Weight*> weights)
     int mapX = (int)position.x;
     int mapY = (int)position.y;
     bool shouldTurn = false;
-    cout << "4: " << this << ' ' << position.x << ' ' << position.y << ' ' << deltaTime << '\n';
 
     movementTimer -= deltaTime;
     if (movementTimer <= 0) {
@@ -138,8 +137,6 @@ void Patroller::update(int deltaTime, const std::vector<Weight*> weights)
     else {
         position.x += moveRight ? speed : -speed;
     }
-
-    cout << "5: " << this << ' ' << position.x << ' ' << position.y << '\n';
 
     this->setPosition(position);
     sprite->update(deltaTime);
